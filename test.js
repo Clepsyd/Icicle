19e6963d5e422c7fd8cd148cef0150e53589fbad
krisswiltshire30 <kriss.wiltshire@googlemail.com>
2019-09-02 15:46:59 +0100

    Added expect and not functionality

--- test.js
+++ test.js
@@ -1,15 +1,23 @@
-function toEqual(a, b){
-  return a === b ? "Pass" : "Fail";
+function toEqual(a, b) {
+  return a === b;
 }
 
 function toContain(sequence, element) {
-  return sequence.includes(element) ? "Pass" : "Fail";
+  return sequence.includes(element);
 }
 
 function Foo() {
   this.bar = "baz";
   this.sequence = [1, 2, 3];
-  this.string = "Mystring"
+  this.string = "Mystring";
+}
+
+function not(a) {
+  return !a;
+}
+
+function expect(boolean) {
+  return boolean ? "Pass" : "Fail";
 }
 
 let foo = new Foo();
@@ -22,4 +30,7 @@ console.log("Should fail: " + toContain(foo.sequence, 4));
 
 console.log("Should pass: " + toContain(foo.string, "Mystring"));
 console.log("Should fail: " + toContain(foo.string, "SNOT"));
-
+console.log("Should be false: " + not(true));
+console.log("Should be true: " + not(false));
+console.log("Should be false: " + expect(not(toContain(foo.sequence, 3))));
+console.log("Should be true: " + expect(not(toContain(foo.sequence, 4))));
