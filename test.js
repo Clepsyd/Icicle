adcb87a02d9227624d3666445c7ad885fa1a61b9
krisswiltshire30 <kriss.wiltshire@googlemail.com>
2019-09-02 15:50:28 +0100

    Added 'toRaiseError' functionality

--- test.js
+++ test.js
@@ -12,6 +12,14 @@ function Foo() {
   this.string = "Mystring";
 }
 
+Foo.prototype.error = function() {
+  throw "This is an error";
+};
+
+Foo.prototype.notAnError = function() {
+  return true;
+};
+
 function not(a) {
   return !a;
 }
@@ -20,17 +28,28 @@ function expect(boolean) {
   return boolean ? "Pass" : "Fail";
 }
 
+function toRaiseError(a) {
+  try {
+    a();
+  } catch (errorMessage) {
+    return true;
+  }
+  return false;
+}
+
 let foo = new Foo();
 
-console.log("Should pass: " + toEqual(foo.bar, "baz"));
-console.log("Should fail: " + toEqual(foo.bar, "plop"));
+console.log("Should pass: " + expect(toEqual(foo.bar, "baz")));
+console.log("Should fail: " + expect(toEqual(foo.bar, "plop")));
 
-console.log("Should pass: " + toContain(foo.sequence, 2));
-console.log("Should fail: " + toContain(foo.sequence, 4));
+console.log("Should pass: " + expect(toContain(foo.sequence, 2)));
+console.log("Should fail: " + expect(toContain(foo.sequence, 4)));
 
-console.log("Should pass: " + toContain(foo.string, "Mystring"));
-console.log("Should fail: " + toContain(foo.string, "SNOT"));
+console.log("Should pass: " + expect(toContain(foo.string, "Mystring")));
+console.log("Should fail: " + expect(toContain(foo.string, "SNOT")));
 console.log("Should be false: " + not(true));
 console.log("Should be true: " + not(false));
-console.log("Should be false: " + expect(not(toContain(foo.sequence, 3))));
-console.log("Should be true: " + expect(not(toContain(foo.sequence, 4))));
+console.log("Should fail: " + expect(not(toContain(foo.sequence, 3))));
+console.log("Should pass: " + expect(not(toContain(foo.sequence, 4))));
+console.log("Should pass: " + expect(toRaiseError(foo.error)));
+console.log("Should fail: " + expect(toRaiseError(foo.notAnError)));
